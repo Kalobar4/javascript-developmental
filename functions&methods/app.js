9f7c23172bffb5af5300dcd8d8978e6a53d5a6fa
Brad Evans <kaloba4r@gmail.com>
2019-09-26 13:41:50 -0400

    Document Object Selector Methods Review

--- functions&methods/app.js
+++ functions&methods/app.js
@@ -1,4 +1,3 @@
-
 /////// WINDOWS / OBJECTS / PROPERTIES  ===  DOM interaction in client side JS window engine using v8 ///////
 
 
@@ -63,24 +62,90 @@ val = document.forms;  // returns an array of all forms in the DOM
 // console.log(val);
 
 
-////  DOM Manipulation //// 
-/// Single Element Selector Methods ///  .getElementById() and 
+////  DOM Manipulation ////  
+/// Single Element Selector Methods on the document object ///  .getElementById() and .querySelector()
+
+let element = document.getElementById('task-title')
 
 // get attributes on a DOM element
 console.log(document.getElementById('task-title').className);
 
 // change style on a DOM element
-document.getElementById('task-title').style.background = '#333'
-document.getElementById('task-title').style.color = '#fff';
-document.getElementById('task-title').style.padding = '5px';
+element.style.background = '#333'
+element.style.color = '#fff';
+element.style.padding = '5px';
+
+// document.getElementById(task - title).style.display = "none";  // to make an element disappear (typically hooked to an event such as onclick etc.)
+
+///  textContent, innerText, innerHTML
+
+element.textContent = `Task List for ${new Date()}`
+
+///  document.querySelector( )
+console.log(document.querySelector('#task-title'));
+console.log(document.querySelector('.card-title'))
+console.log(document.querySelector('h2'))  // will select by . # or by element name.  But will only return the first element matching criteria  //  bc This is a single element selector.
+// you can also select nested elements
+document.querySelector('ul li').style.color = 'blue';
+// and use pseudo to select parent/child elements
+// document.querySelector('li: last-child').style.color = 'blue'
+// specify nth child using css 3
+// document.querySelector('li: nth-child(3)').style.color = 'green';
+// document.querySelector('li: nth-child(4)').textContent = '4th element new child name';
 
 
+////  DOM Manipulation //// 
+/// Multi Element Selector Methods on the document object ///  .getElementsByClassName() and .getElementsByTagName()
+
+// const items = document.getElementsByClassName("card-title");
+// console.log(items)
+// console.log(items[0])
+// items[0].style.color = 'green';
+// items[3].textContent = '4th element new child name'
+
+const listItems = document.querySelector('ul').getElementsByClassName('card-title')
+
+console.log(listItems, 'limited range selector')
+
+// document.getElementsByTagName
+let lis = document.getElementsByTagName("li");
+console.log(lis)
+console.log(lis[0])
+lis[0].style.color = 'blue';
+lis[3].textContent = '4th element new child name'
+
+// Note However that html collections are not arrays  - so you will get xxx is not a function if you try and use array methods on it.
+
+//
+lis = Array.from(lis)  //  to convert array-obj into true array
+lis.reverse()  // execute array reverse method on it
+console.log(lis);
+
+// lis.forEach(function(li){
+//   console.log(li.className);
+//   li.textContent = `Index: ${index} now reversed order`
+// })   // to loop through list items and change their content...
 
 
+/////  QuerySelectorAll
 
+const items = document.querySelectorAll('ul.collection.li.collection-item')  // same as li now output as a node list
+console.log(items);
+// array method on node list
+items.forEach(function (item) {
+  // item.textContent = `Index: ${index} now reversed order`;
+  console.log(item)
+})
 
+const liOdd = document.querySelectorAll('li:nth-child(odd)');
+const liEven = document.querySelectorAll('li:nth-child(even)')
 
-
+liOdd.forEach(function (li, index) {
+  li.style.background = '#ccc'
+});
+for (let i = 0; i < liEven.length; i++) {
+  liEven[i].style.backgroun = '#f4f4f4'
+};
 
 
 
@@ -132,60 +197,60 @@ document.getElementById('task-title').style.padding = '5px';
 
 ///  LOOPS /// FOR, WHILE, DO WHILE and FOR IN
 
-// continue
-for (let i = 0; i < 10; i++) {
-  if (i === 2) {
-    console.log('Two is my favorite number, but I won\'t print it');
-    continue;    //  continue returns that iteration w o executing anything further for that round
-  }
-  console.log('Number :' + i);
-}
-
-// while  // only condition goes in (); increment should be inside execution block.  === best to use a while loop when you don't know for certain the number of iterations that you will need.  a for loop when it is defined.
-// let i = 0
-// while (i < 10) {
-//   console.log('Number ' + i);
-//   i++
-// };
-
-// do while -- same as while loop but always runs 1x
-// let i = 0;
-// do {
-//   console.log('Number: ' + i)
+// // continue
+// for (let i = 0; i < 10; i++) {
+//   if (i === 2) {
+//     console.log('Two is my favorite number, but I won\'t print it');
+//     continue;    //  continue returns that iteration w o executing anything further for that round
+//   }
+//   console.log('Number :' + i);
 // }
-// while (i < 10);
+
+// // while  // only condition goes in (); increment should be inside execution block.  === best to use a while loop when you don't know for certain the number of iterations that you will need.  a for loop when it is defined.
+// // let i = 0
+// // while (i < 10) {
+// //   console.log('Number ' + i);
+// //   i++
+// // };
+
+// // do while -- same as while loop but always runs 1x
+// // let i = 0;
+// // do {
+// //   console.log('Number: ' + i)
+// // }
+// // while (i < 10);
 
 
 
 
-// Array for loops
-const cars = ['ford', 'chevy', 'mazda', 'toyota', 'bmw']
+// // Array for loops
+// const cars = ['ford', 'chevy', 'mazda', 'toyota', 'bmw']
 
-/// LOOP THROUGH AN ARRAY
-/// For Loop
-// for (i = 0; i < cars.length; i++) {
-//   console.log(cars[i])
-// };
+// /// LOOP THROUGH AN ARRAY
+// /// For Loop
+// // for (i = 0; i < cars.length; i++) {
+// //   console.log(cars[i])
+// // };
 
-///For Each Loop - takes in a cb to execute on each item
+// ///For Each Loop - takes in a cb to execute on each item
 
-// cars.forEach(function (car, index, array) {
-//   console.log(`Index ${index}: ${car}`)
-//   console.log(array)
-// })
+// // cars.forEach(function (car, index, array) {
+// //   console.log(`Index ${index}: ${car}`)
+// //   console.log(array)
+// // })
 
-////  For In Loop for Objects
-const user = {
-  firstName: 'John',
-  lastName: 'Doe',
-  age: 40
-}
+// ////  For In Loop for Objects
+// const user = {
+//   firstName: 'John',
+//   lastName: 'Doe',
+//   age: 40
+// }
 
-for (let x in user) {
-  console.log(`key is ${x}`)  // x is the key
-  console.log(`value is ${user[x]}`)  // x is the value
-  console.log(`object is ${x} : ${user[x]}`)
-}
+// for (let x in user) {
+//   console.log(`key is ${x}`)  // x is the key
+//   console.log(`value is ${user[x]}`)  // x is the value
+//   console.log(`object is ${x} : ${user[x]}`)
+// }
 
 
 
