25e791db32e3f78be3370436fddee89bf9d5c661
Brad Evans <kaloba4r@gmail.com>
2019-09-26 11:59:15 -0400

    DOM manipulation review1

--- functions&methods/app.js
+++ functions&methods/app.js
@@ -1,3 +1,96 @@
+
+/////// WINDOWS / OBJECTS / PROPERTIES  ===  DOM interaction in client side JS window engine using v8 ///////
+
+
+/// DOM Dimensions
+let val;
+val = window.outerHeight;
+val2 = window.outerWidth;
+val3 = window.innerHeight;
+val4 = window.innerWidth;
+
+val5 = window.scrollY  // <div style = "margin-top: 1000px"></div>
+val6 = window.scrollX  // <div style = "margin-left: 1000px"></div>   // good for those websites when you scroll down and have animations to let you know where the user is.
+
+val = window.location;
+val = window.location.hostname
+val = window.location.port
+val = window.location.href
+///  User query parameters to interact!!!
+val = window.location.search  //  the user's query info ?id =1&name=Brad&member=standard etc.
+
+/// Using the window object to reload and redirect w in a function (not globally or it will keep refreshing)
+//  window.location.reload()
+// window.location.href = 'http://google.com'
+
+// window.history.go(-1);  
+// val = window.history.length;
+
+/// Navigator Object /// geolocation, userAgent browser type, .platform  - Darwin for Mac win32 for Windows
+
+
+///  Window.document properties
+val = document;
+console.log(val);
+
+// html collection, node lists 
+val = document.all
+console.log(val);
+val = document.body;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+val = document.contentType;
+val = document.forms;  // returns an array of all forms in the DOM
+// val = document.forms[0].method  // get post etc
+// val = document.forms[0].action
+// val = document.forms[0].id
+
+// val = document.links  // array of links in the DOM
+// console.log(val);
+// val = document.links[0].id
+// val = document.links[0].className
+// val = document.links[0].classList
+
+// val = document.scripts // array of scripts in an window DOM
+// val = document.scripts[1]
+// val = document.scripts[1].getAttribute('src')
+// let scripts = document.scripts;
+// let scriptsArr = Array.from(scripts);
+// scriptsArr.forEach(function (script) {
+//   console.log(script.getAttribute('src'))
+// })
+// console.log(val);
+
+
+////  DOM Manipulation //// 
+/// Single Element Selector Methods ///  .getElementById() and 
+
+// get attributes on a DOM element
+console.log(document.getElementById('task-title').className);
+
+// change style on a DOM element
+document.getElementById('task-title').style.background = '#333'
+document.getElementById('task-title').style.color = '#fff';
+document.getElementById('task-title').style.padding = '5px';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////  FUNCTION REVIEW  ///////////
+
+
+
 // //  Function Declarations
 // function greet(firstName = 'John', lastName = 'Doe') {
 //   return 'Hello ' + firstName + ' ' + lastName;
@@ -93,6 +186,14 @@ for (let x in user) {
   console.log(`value is ${user[x]}`)  // x is the value
   console.log(`object is ${x} : ${user[x]}`)
 }
+
+
+
+
+
+
+
+
 
 
 
